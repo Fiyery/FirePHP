456eea65b39d44e944cdfdf470533b9c366f8c01
yoann <yoann.chaumin@gmail.com>
2016-07-21 22:09:32 +0200

    1.0.4 - Helper pour Tableau PHP

--- app/lib/Table.class.php
+++ app/lib/Table.class.php
@@ -1,11 +1,44 @@
 <?php
+/**
+ * Helper pour l'utilisation des Tableaux.
+ * @author Yoann Chaumin <yoann.chaumin@gmail.com>
+ */
 class Table implements ArrayAccess, IteratorAggregate, Countable
 {
+	/**
+	 * Identifiant CSS du tableau si génération HTML.
+	 * @var string
+	 */
     private $_id = '';
+    
+    /**
+     * Classes CSS du tableau si génération HTML.
+     * @var string
+     */
     private $_class = '';
+    
+    /**
+     * Liste des entêtes du tableau si génération HTML.
+     * @var array
+     */
     private $_header = [];
+    
+    /**
+     * Contenu du tableau.
+     * @var array
+     */
     private $_array;
+    
+    /**
+     * Tableau parent afin de permettre la modification des sous-éléments d'un tableau.
+     * @var Table
+     */
     private $_parent = NULL;
+    
+    /**	
+     * Tableaux enfant afin de permettre la modification des sous-éléments d'un tableau.
+     * @var unknown
+     */
     private $_childs = [];
 
     /**
@@ -19,11 +52,15 @@ class Table implements ArrayAccess, IteratorAggregate, Countable
 
     /**
      * Retourne une valeur selon la clé.
-     * @param string $index
+     * @param string $index NULL pour récupérer tout le tableau.
      * @return mixed
      */
-    public function get($index)
+    public function get($index=NULL)
     {
+        if ($index === NULL)
+        {
+            return $this->_array;
+        }
         if ($this->has($index))
         {
             $res = $this->_array[$index];
@@ -79,6 +116,107 @@ class Table implements ArrayAccess, IteratorAggregate, Countable
     public function search($value)
     {
         return array_search($value, $this->_array);
+    }
+
+    /**
+     * Trie le tableau.
+     * @param bool $asc Défini si l'ordre est croissant ou non.
+     * @param string
+     * @return array
+     */
+    public function sort($asc=TRUE, $column=NULL)
+    {
+        uasort($this->_array, function($a, $b) use ($column, $asc) {
+            return $this->_compare($a, $b, $asc, $column);
+        });
+        return $this;
+    }
+
+    /**
+     * Trie le tableau selon l'algorithme du trie à bulle.
+     * @param bool $asc Défini si l'ordre est croissant ou non.
+     * @param string
+     * @return array
+     */
+    public function sort_linear($asc=TRUE, $column=NULL)
+    {
+        $max = $this->count();
+        for($j=1; $j < $max; $j++)
+        {
+            for($i=1; $i < $max; $i++)
+            {
+                if ($this->_compare($this->_array[$i-1], $this->_array[$i], $asc, $column) > 0)
+                {
+                    $o = $this->_array[$i-1];
+                    $this->_array[$i-1] = $this->_array[$i];
+                    $this->_array[$i] = $o;
+                }
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Compare deux éléments
+     * @param mixed $a Premier élément à comparer.
+     * @param mixed $b Deuxième élément à comparer.
+     * @param bool $asc Trie croissant ou décroissant.
+     * @param string $column Nom de la colonne des sous-éléments si besoin.
+     * @return int
+     */
+    private function _compare($a, $b, $asc=TRUE, $column=NULL)
+    {
+        if ($column !== NULL)
+        {
+            if ((is_object($a) && isset($a->$column) === FALSE) || (is_array($a) && isset($a[$column]) === FALSE) || (is_object($a) === FALSE && is_array($a) === FALSE))
+            {
+                return 0;
+            }
+            if ((is_object($b) && isset($b->$column) === FALSE) || (is_array($b) && isset($b[$column]) === FALSE) || (is_object($b) === FALSE && is_array($b) === FALSE))
+            {
+                return 0;
+            }
+            $a = (is_object($a)) ? ($a->$column) : ($a[$column]);
+            $b = (is_object($b)) ? ($b->$column) : ($b[$column]);
+        }
+        if ($a == $b)
+        {
+            return 0;
+        }
+        elseif (is_numeric($a) && is_numeric($b))
+        {
+            return (($asc && $a > $b) || ($asc === FALSE && $a < $b)) ? (1) : (-1);
+        }
+        elseif ((is_scalar($a) && is_scalar($b)))
+        {
+            return ($asc) ? (strcasecmp($a, $b)) : (strcasecmp($b, $a));
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    /**
+     * Exécute une fonction sur tout le table ou une colonne des sous-éléments.
+     * @param callback $callback Fonction a exécuter.
+     * @param string $column Colonne sur laquelle exécuter la fonction pour les sous-éléments.
+     */
+    public function call($callback, $column = NULL)
+    {
+        if ($column === NULL)
+        {
+            array_walk($this->_array, $callback);
+        }
+        else
+        {
+            reset($this->_array);
+            while (list($i, $o) = each($this->_array))
+            {
+                $this->_array[$i][$column] = call_user_func($callback, $this->_array[$i][$column]);
+            }
+        }
+        return $this;
     }
 
     /**
